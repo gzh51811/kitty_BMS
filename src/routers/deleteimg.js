bffdb31f49afc124e1fe37b1aa9f95b53d51a52e
simidaliang <496864759@qq.com>
2019-03-05 11:15:10 +0800

    更新：商品列表，添加；订单列表

---
+++ src/routers/deleteimg.js
@@ -0,0 +1,39 @@
+const Router = require('koa-router');
+
+const db = require('../db');
+
+
+// 引入文件模块
+const fs = require('fs');
+
+// 创建路由
+var router = new Router();
+
+
+/**
+ * ctx
+ */
+router.get('/', async (ctx, next) => {
+    console.log(ctx.request.query)
+    let { img } = ctx.request.query;
+    console.log('url', img);
+    var url = '../' + img
+
+    fs.unlink(img, (err) => {
+        if (err) throw err;
+        // console.log('文件已删除');
+    });
+    ctx.body = {
+        "code": 0
+        , "msg": "删除图片成功"
+
+    }
+
+
+});
+
+
+
+module.exports = router;
+
+
